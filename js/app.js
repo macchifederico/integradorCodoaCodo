2fda242238ea0b127aaf56db11c024e5a3d1da0c
macchifederico <federico.macchi@gmail.com>
2022-11-07 15:31:32 -0300

    Primer commit con la parte de los tickets

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@ const valorTicket = 200;
 const estudiante = document.getElementsByTagName('h5');
 const formulario = document.getElementById('form');
 
-
+const expresiones = {
+    nombre: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/	
+}
 
 
 resumen.addEventListener('click', () => {
@@ -41,9 +45,34 @@ borrar.addEventListener('click', () => {
 
 const validarCampos = (e) => {
     
-    switch (e.target.nombre) {
+    switch (e.target.name) {
+        
         case "nombre":
-            console.log(e);
+            if(expresiones.nombre.test(e.target.value)){
+                document.getElementById('nombre').classList.remove('border-danger');
+                document.getElementById('nombre').classList.add('border-success');
+            } else {
+                document.getElementById('nombre').classList.add('border-danger');
+                document.getElementById('nombre').classList.remove('border-success');
+            }
+        break;
+        case "apellido":
+            if(expresiones.apellido.test(e.target.value)){
+                document.getElementById('apellido').classList.remove('border-danger');
+                document.getElementById('apellido').classList.add('border-success');
+            } else {
+                document.getElementById('apellido').classList.add('border-danger');
+                document.getElementById('apellido').classList.remove('border-success');
+            }        
+        break;
+        case "email":
+            if(expresiones.email.test(e.target.value)){
+                document.getElementById('email').classList.remove('border-danger');
+                document.getElementById('email').classList.add('border-success');
+            } else {
+                document.getElementById('email').classList.add('border-danger');
+                document.getElementById('email').classList.remove('border-success');
+            }        
         break;
     }
 }
@@ -55,7 +84,6 @@ inputs.forEach((input) => {
 })
 
 formulario.addEventListener('submit', (e) => {
-    console.log('toque sumbit');
     e.preventDefault();
 })
 
